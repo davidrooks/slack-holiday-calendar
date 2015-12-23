b429697756308001d9703b5f9c97245ec295f378
dro03 <david.rooks@bskyb.com>
2015-12-23 15:36:11 +0000

    added ability to add range of dates

--- app.rb
+++ app.rb
@@ -38,32 +38,59 @@ end
 post '/' do
   if params['text']
     commands = params['text'].split
-    if commands.size == 2
+    if commands.size >= 2
       slack_name = commands[0]
       holiday_date = commands[1]
-      # if !name.start_with? '@'
-      #   showUsage
-      # end
-      # if !
-      addHoliday(slack_name.to_sym, holiday_date)
+      if !slack_name.start_with? '@'
+        showUsage
+      end
+      begin
+        Date.parse(holiday_date)
+      rescue ArgumentError
+        showUsage
+      end
+      if commands.size == 2
+        addHoliday(slack_name.to_sym, holiday_date)
+      end
+      if commands.size == 4 && commands[2].upcase == 'TO'
+        holiday_date_end = commands[3]
+        begin
+          Date.parse(holiday_date_end)
+        rescue ArgumentError
+          showUsage
+        end
+        addHoliday(slack_name.to_sym, holiday_date, holiday_date_end)
+      end
+
     end
   end
 
   showHolidays
 end
 
-def addHoliday(name, date) 
-  puts @holidays.to_s
-  if @holidays.has_key? name
-    puts 'key exists'
-    if !@holidays[name].include? date
-      @holidays[name] << date
-    end
-  else
-    puts 'new key'
-    @holidays[name] = []
-    @holidays[name] << date
+def addHoliday(name, start_date, end_date=NIL) 
+  if end_date.nil? 
+    end_date=start_date
   end
+
+  if !@holidays.has_key? name
+    @holidays[name] = []
+  end
+
+  Date.parse(start_date).upto(Date.parse(end_date)) do |date|
+    puts 'adding ' + date.strftime('%d-%m-%Y')
+    if !@holidays[name].include? date.strftime('%d-%m-%Y')
+      @holidays[name] << date.strftime('%d-%m-%Y')
+    end
+  end
+end
+
+def showUsage
+  res = {}
+  res['title'] = 'Holidays'
+  res['text'] = '*USAGE*\nTo view the upcoming holidays calendar just type /holidays\n
+  To add a date to the calendar type /holidays [@name] [dd-mm-yyyy]   E.G. /holidays @john 25-12-2016\n'
+  halt 200, {'Content-Type' => 'application/json'}, res.to_json
 end
 
 def showHolidays
@@ -100,7 +127,7 @@ end
 
 
 after do
-  @holidays.each {|k,v| @holidays[k] = v.reject{|x| Date.strptime(x,'%d-%m-%Y')<Date.today}}
+  @holidays.each {|k,v| @holidays[k] = v.reject{|x| Date.parse(x) < Date.today}}
   @res[:json] = @holidays.to_json
   @res.save
 end
