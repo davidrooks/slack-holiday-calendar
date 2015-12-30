cbe62abd544f76e88520706e5ea9494d10e89f21
dro03 <david.rooks@bskyb.com>
2015-12-30 09:38:23 +0000

    changed json response to work around formatting issues

--- app.rb
+++ app.rb
@@ -38,9 +38,9 @@ end
 post '/' do
   if params['text']
     commands = params['text'].split
-    if commands.size >= 2
-      slack_name = commands[0]
-      holiday_date = commands[1]
+    if commands.size >= 3
+      slack_name = commands[1]
+      holiday_date = commands[2]
       if !slack_name.start_with? '@'
         showUsage
       end
@@ -49,24 +49,43 @@ post '/' do
       rescue ArgumentError
         showUsage
       end
-      if commands.size == 2
-        addHoliday(slack_name.to_sym, holiday_date)
-      end
-      if commands.size == 4 && commands[2].upcase == 'TO'
-        holiday_date_end = commands[3]
-        begin
-          Date.parse(holiday_date_end)
-        rescue ArgumentError
-          showUsage
-        end
-        addHoliday(slack_name.to_sym, holiday_date, holiday_date_end)
-      end
 
+      if commands[0].upcase == 'ADD'
+        if commands.size == 3 
+          addHoliday(slack_name.to_sym, holiday_date)        
+        end
+        if commands.size == 5 && commands[3].upcase == 'TO'
+          holiday_date_end = commands[3]
+          begin
+            Date.parse(holiday_date_end)
+          rescue ArgumentError
+            showUsage
+          end
+          addHoliday(slack_name.to_sym, holiday_date, holiday_date_end)
+        end
+      elsif commands[0].upcase == 'REMOVE'        
+        if commands.size == 3 
+          deleteHoliday(slack_name.to_sym, holiday_date)        
+        end
+        if commands.size == 5 && commands[3].upcase == 'TO'
+          holiday_date_end = commands[3]
+          begin
+            Date.parse(holiday_date_end)
+          rescue ArgumentError
+            showUsage
+          end
+          deleteHoliday(slack_name.to_sym, holiday_date, holiday_date_end)
+        end
+      end
     end
   end
 
   showHolidays
 end
+
+def validateParams()
+end
+
 
 def addHoliday(name, start_date, end_date=NIL) 
   if end_date.nil? 
@@ -84,12 +103,27 @@ def addHoliday(name, start_date, end_date=NIL)
   end
 end
 
+def deleteHoliday(name, start_date, end_date=NIL) 
+  if end_date.nil? 
+    end_date=start_date
+  end
+
+  if @holidays.has_key? name
+    Date.parse(start_date).upto(Date.parse(end_date)) do |date|    
+      @holidays[name].delete(date.strftime('%d-%m-%Y'))        
+    end
+  end
+end
+
+
 def showUsage
   res = {}
   res['title'] = 'Holidays'
   res['text'] = '*USAGE*\nTo view the upcoming holidays calendar just type /holidays\n
-  To add a date to the calendar type /holidays [@name] [dd-mm-yyyy]   E.G. /holidays @john 25-12-2016\n
-  To add a rangeof dates type /holidays [@name] [dd-mm-yyyy] to [dd-mm-yyyy]   E.G. /holidays @john 22-12-2015 to 03-01-2016\n'
+  To add a date to the calendar type /holidays add [@name] [dd-mm-yyyy]   E.G. /holidays @john 25-12-2016\n
+  To add a rangeof dates type /holidays add [@name] [dd-mm-yyyy] to [dd-mm-yyyy]   E.G. /holidays @john 22-12-2015 to 03-01-2016\n
+  To delete a date from the calendar type /holidays remove [@name] [dd-mm-yyyy]   E.G. /holidays remove @john 25-12-2016\n
+  To delete a rangeof dates type /holidays add remove [@name] [dd-mm-yyyy] to [dd-mm-yyyy]   E.G. /holidays remove @john 22-12-2015 to 03-01-2016\n'
   halt 200, {'Content-Type' => 'application/json'}, res.to_json
 end
 
@@ -99,14 +133,18 @@ def showHolidays
     res["response_type"] = "in_channel"
   end
   res["title"] = "Holidays"
-  tmp = ''
+  res['attachments'] = []
+  
 
 
   Date.today.upto(Date.today+14.days) do |date|
-    tmp = tmp + '*' + date.strftime('%d-%m-%Y') + '* -- ' + checkDate(date.strftime('%d-%m-%Y')) + '\n'
+    tmp = {}
+    tmp['title'] = date.strftime('%d-%m-%Y') 
+    tmp['text'] = checkDate(date.strftime('%d-%m-%Y'))
+    tmp['color'] = 'good'
+    res['attachments'] << tmp
   end
 
-  res["text"] = tmp
   res['mrkdwn'] = true
 
   halt 200, {'Content-Type' => 'application/json'}, res.to_json
